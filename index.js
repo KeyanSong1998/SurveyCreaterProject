443db8eec64db25b39d77da0b7bac7fde8dab2dc
KeyanSong1998 <kxs687@cas.edu>
2021-06-11 03:07:57 -0700

    changed greeting

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ const app = express();
     // '/' represent the route of the visit (浏览器里的地址)
     //
 
-app.get('/',() =>{
-
+app.get('/',(req,res) =>{
+    res.send({bye:'dad'});
 });
 
+
+//heroku open,  heroku logs, git push heroku master
+// https://frozen-ravine-23768.herokuapp.com/ | https://git.heroku.com/frozen-ravine-23768.git
 //This is for listen to dynamic routes from heroku.
 //Heroku can inject enviroment variables 
 const PORT = process.env.PORT|| 5000;  // default is 5000 (under development local machine)
 app.listen(PORT);
+
+
 
